32c9c9eb47478fd1ac489497ec18f789fdb722da
Ghost-Element <danieldw@edu.aau.at>
2025-06-10 15:31:40 +0200

    Fixed Heart Alignment Problem
    Basically set Heart vertical Anchor on minimize (V Center Wide)

--- anastasis/scenes/heart.gd
+++ anastasis/scenes/heart.gd
@@ -3,12 +3,12 @@ extends Control
 @onready var sprite =$AnimatedSprite2D
 
 func _ready():
-	_rescale_and_center()
+	rescale_and_center()
 
 func _resized():
-	_rescale_and_center()
+	rescale_and_center()
 
-func _rescale_and_center():
+func rescale_and_center():
 	# Error case: if animation isn´t set
 	if not sprite.sprite_frames.has_animation(sprite.animation):
 		return
@@ -19,9 +19,6 @@ func _rescale_and_center():
 		return
 	var frame_size = frame_texture.get_size()
 	var target_size = size
-	print("Size: "+str(size))
-	if(size.x != size.y):
-		size.x = size.y
 	# Calculate uniform scale factor (preserve aspect ratio)
 	var scale_factor = min(target_size.x / frame_size.x, target_size.y / frame_size.y)
 	sprite.scale = Vector2.ONE * scale_factor
@@ -30,7 +27,3 @@ func _rescale_and_center():
 	#var scaled_sprite_size = frame_size * scale_factor
 	#(63.33506, 64.0) of (64.0, 64.0)
 	sprite.position = size / 2.0 
-
-
-	var hBoxContainer = get_parent()
-	print("Heart: "+str(hBoxContainer.position))
