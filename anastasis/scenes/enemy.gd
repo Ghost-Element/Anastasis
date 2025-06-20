ac98f1d2ebd220a990eeb67512f2517b03e47842
Aaron Wagner <Aaron.Wagner@edu.fh-kaernten.ac.at>
2025-06-20 18:29:24 +0200

    Enemy Charge Attack
    enemy will charge at you and deal 3 damage! dodge to the side!

--- anastasis/scenes/enemy.gd
+++ anastasis/scenes/enemy.gd
@@ -1,28 +1,40 @@
 extends CharacterBody2D
 @export var speed := 30000
 @export var target_node: Node
-@export var follow_range: float = 800
+@export var follow_range: float = 1200
+@export var charge_range: float = 700
 @export var fighting_range: float = 400
-var distance: float = 1000
+var distance: float
 var direction: Vector2 = Vector2.DOWN
 @export var attack_hitbox: PackedScene
 
 var idle_timer: float = 0
 var attack_timer: float = 0
 
+var charge_timer: float = 0
+var has_charge_hitbox: bool = false
+var is_charge_interrupted: bool = false
+var charge_wait_time: float = 0.7
+var charge_charge_time: float = 0.8
+var charge_break_time: float = 0.2
+
 # State Machine Variables
-enum State { IDLE, FOLLOW, FIGHTING }
+enum State { IDLE, FOLLOW, CHARGE, FIGHTING }
 var state = State.IDLE
 # possible state changes:
 # IDLE -> FOLLOW: if distance < follow_range
 # FOLLOW -> IDLE: if distance > follow_range
-# FOLLOW -> FIGHTING: if distance < fighting_range
-# FIGHTING -> FOLLOW: if distance > fighting_range
+# FOLLOW -> CHARGE: if distance < charge_range
+# CHARGE -> ANY: if charge attack is done
+# FIGHTING -> CHARGE: if distance > fighting_range
+# FOLLOW -> FIGHTING: if distance < fighting_range # if player dashes too close to charge
+# FIGHTING -> FOLLOW: if distance > charge_range # if player dashes too far to charge
 
 func _ready():
 	# enable to randomize seed
 	# randomize()
 	pass
+
 
 func _physics_process(_delta):
 	distance = global_position.distance_to(target_node.global_position)
@@ -37,6 +49,9 @@ func _physics_process(_delta):
 			handle_follow(_delta)
 		State.FIGHTING:
 			handle_fighting(_delta)
+		State.CHARGE:
+			handle_charge(_delta)
+
 
 func take_damage(dmg: int, sender):
 	print("%s took %d damage from %s" % [self.to_string(), dmg, sender])
@@ -44,6 +59,10 @@ func take_damage(dmg: int, sender):
 
 func set_target(target: Node):
 	target_node = target
+
+func interrupt_charge():
+	is_charge_interrupted = true
+
 
 func handle_idle(_delta: float):
 	idle_timer += _delta
@@ -65,6 +84,7 @@ func handle_idle(_delta: float):
 		state = State.FOLLOW
 		print("following")
 
+
 func handle_follow(_delta: float):
 	direction = (target_node.global_position - global_position).normalized()
 	velocity = direction * speed * _delta
@@ -75,11 +95,17 @@ func handle_follow(_delta: float):
 		# transition_to_idle()
 		state = State.IDLE
 		print("idling")
-	if distance < fighting_range:
+	elif distance < fighting_range:
 		# transition_from_follow()
 		# transition_to_fighting()
 		state = State.FIGHTING
 		print("fighting")
+	elif distance < charge_range:
+		# transition_from_follow()
+		# transition_to_charge()
+		state = State.CHARGE
+		print("charging")
+
 
 func handle_fighting(_delta: float):
 	look_at(target_node.global_position)
@@ -94,9 +120,73 @@ func handle_fighting(_delta: float):
 		hitbox.init_hitbox(Vector2(150,150), Vector2(1,0), Vector2(250,0))
 		add_child(hitbox)
 	
-	if distance > fighting_range:
+	if distance > charge_range:
 		# transition_from_fighting()
 		attack_timer = 0
 		# transition_to_follow()
 		state = State.FOLLOW
 		print("following2")
+	elif distance > fighting_range:
+		# transition_from_fighting()
+		# transition_to_charge()
+		state = State.CHARGE
+		print("charging")
+
+
+func handle_charge(_delta: float):
+	charge_timer += _delta
+	# stare phase
+	if charge_timer < charge_wait_time:
+		look_at(target_node.global_position)
+		velocity = Vector2.ZERO
+		move_and_slide()
+		if fighting_range > distance or distance > follow_range:
+			pass # if too close or too far, change state by not returning
+		else:
+			return
+	elif is_charge_interrupted:
+		velocity = velocity*0.05
+		move_and_slide()
+		is_charge_interrupted = false
+		# allow state transition by not returning
+	elif charge_timer < (charge_wait_time + charge_charge_time):
+		if not has_charge_hitbox:
+			var hitbox = attack_hitbox.instantiate()
+			hitbox.init_hitbox(Vector2(180,120), Vector2(1,0), Vector2(150,0), 0.9, 3)
+			add_child(hitbox)
+			has_charge_hitbox = true
+		direction = (target_node.global_position - global_position).normalized()
+		velocity += direction * speed*0.13 * _delta
+		move_and_slide()
+		return
+	elif charge_timer < (charge_wait_time + charge_charge_time + charge_break_time):
+		velocity *= 0.95
+		move_and_slide()
+		return
+		
+	# Check for state change
+	if distance > follow_range:
+		transition_from_charge()
+		# transition_to_idle()
+		state = State.IDLE
+		print("idling")
+	elif distance < follow_range:
+		transition_from_charge()
+		#transition_to_follow
+		state = State.FOLLOW
+		print("following")
+	elif distance < fighting_range:
+		transition_from_charge()
+		# transition_to_fighting()
+		state = State.FIGHTING
+		print("fighting")
+	elif distance < charge_range:
+		transition_from_charge()
+		# transition_to_charge()
+		state = State.CHARGE
+		print("charging")
+
+func transition_from_charge():
+	charge_timer = 0;
+	has_charge_hitbox = false
+	is_charge_interrupted = false
--- anastasis/scenes/enemy_attack_hitbox.gd
+++ anastasis/scenes/enemy_attack_hitbox.gd
@@ -6,27 +6,34 @@ extends Area2D
 @export var damage: int = 1
 @export var lifetime: float = 0.4
 
-#always call init_hitbox after initializing hitbox
-func _ready() -> void:
-	# Connect to body_entered or area_entered if needed
-	self.connect("body_entered", _on_body_entered)
-	await get_tree().create_timer(lifetime).timeout
-	("deleted hitbox")
-	queue_free()
-
 #orientation is a vector pointing in a direction (e.g. Vector2.DOWN)
 #position.x = distance from player center forward, position.y = distance from player center perpendicular (right=positive)
-func init_hitbox(size:Vector2, orientation:Vector2, position:Vector2):
-	print("enabled hitbox")
+func init_hitbox(size:Vector2, orientation:Vector2, position:Vector2, lifetime:float = 0.4, dmg:int = 1):
 	$CollisionShape2D.disabled = false
 	$CollisionShape2D.shape = RectangleShape2D.new()
 	$CollisionShape2D.shape.extents = size
 	self.position = position.x*orientation + position.y*orientation.rotated(90)
 	self.rotation = Vector2.DOWN.angle_to(orientation)
+	self.damage = dmg
+	self.lifetime = lifetime
+	self.connect("body_entered", _on_body_entered)
+	print("enabled hitbox")
 	#transform = transform.translated(position).looking_at(orientation)
+	
+	# wait until finished initialisation (otherwise get_tree() returns null)
+	call_deferred("post_init_hitbox")
+
+func post_init_hitbox():
+	await self.get_tree().create_timer(lifetime).timeout
+	print("deleted hitbox")
+	queue_free()
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		body.take_damage(damage, self)  # Ensure enemy has this method
+		if get_parent() and get_parent().has_method("interrupt_charge"):
+			get_parent().interrupt_charge()
+			queue_free()
+		self.get_parent()
 	if body is TileMapLayer:
 		print("%s is hitting terrain", [self])
